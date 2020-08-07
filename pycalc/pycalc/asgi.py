ff1937851c0b7510f10f55f96755e8cfd1e52cfb
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-07 13:13:01 -0500

    resolved version dependancies in asgi.py

--- pycalc/pycalc/asgi.py
+++ pycalc/pycalc/asgi.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 import django # added
-from channels.asgi import get_channel_layer # added
 from django.core.asgi import get_asgi_application
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pycalc.settings')
-# added below  (and import channels.asgi) via  https://blog.heroku.com/in_deep_with_django_channels_the_future_of_real_time_apps_in_django
-channel_layer = get_channel_layer()
-#django.setup() # added
-#application = get_asgi_application()
+
+django.setup() # added
+application = get_asgi_application()
 
