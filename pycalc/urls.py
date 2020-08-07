8d25f5d86b64bb80648691c7ba6f6f180e528642
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-07 14:27:00 -0500

    Resovling url -> calculator dependancy for heroku...

--- pycalc/urls.py
+++ pycalc/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from calculator import views
+from pycalc.calculator import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('user/resultingcalc/', views.new_calc),
