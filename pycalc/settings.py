e9d7f8d1f5cd3f1e41338920decfef30b0351e41
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-07 22:56:57 -0500

    settings error fixed

--- pycalc/settings.py
+++ pycalc/settings.py
@@ -92,7 +92,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts":  [("redis_host", 6379)],,
+            "hosts":  [("redis_host", 6379)],
         },
     },
 }
