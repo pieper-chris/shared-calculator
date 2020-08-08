09c8677b08274de639c99d04a8fce0777f18bb56
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-07 23:25:17 -0500

    settings changes

--- pycalc/settings.py
+++ pycalc/settings.py
@@ -151,7 +151,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Activate Django-Heroku.
 django_heroku.settings(locals())
