f1d9edc626daeb80334b217adcdf48e4b12d02d4
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-08 10:56:18 -0500

    Installed redis for heroku, updated settings

--- pycalc/settings.py
+++ pycalc/settings.py
@@ -93,8 +93,9 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDISTOGO_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDISCLOUD_URL', 'redis://localhost:6379')],
         },
+        "ROUTING": "pycalc.routing.application",
     },
 }
 
