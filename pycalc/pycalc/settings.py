b206391abb023daf087304195e3f9a94e6dcf518
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-08 17:06:21 -0500

    debugging server error

--- pycalc/pycalc/settings.py
+++ pycalc/pycalc/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7w*=g7sapd*qzrq_a-3jexsq-ihk@m9-r=gohy%fv&p-e@@ha%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
