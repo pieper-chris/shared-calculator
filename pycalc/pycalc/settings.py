c398eef029b7a0f29aacbd68ba3a946d31428d7f
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-08 17:09:34 -0500

    secret key added for build

--- pycalc/pycalc/settings.py
+++ pycalc/pycalc/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+SECRET_KEY = 'not_actually_the_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
