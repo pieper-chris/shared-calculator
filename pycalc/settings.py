714e7a0579f8086b7cdc85124988ab1ee3d50cc5
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-09 12:57:37 -0500

    adding whitenoise setting

--- pycalc/settings.py
+++ pycalc/settings.py
@@ -189,6 +189,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Activate Django-Heroku.
