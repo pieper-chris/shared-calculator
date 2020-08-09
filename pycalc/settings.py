86d98df24c4d05925104721f483cb0fc55e833c7
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-09 10:29:43 -0500

    new changes to templates (take 5) & settings update

--- pycalc/settings.py
+++ pycalc/settings.py
@@ -177,11 +177,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+'''STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)'''
 
 STATIC_URL = '/static/'
 
