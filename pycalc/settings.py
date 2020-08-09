c7e46fbcc6c50cade341a1aeeab2833c9bd186b6
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-08-08 19:45:52 -0500

    changed static_url path in settings

--- pycalc/settings.py
+++ pycalc/settings.py
@@ -180,10 +180,10 @@ STATIC_TMP = os.path.join(BASE_DIR, 'static')
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-STATIC_URL = '/static/'
+STATIC_URL = '/pycalc/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pycalc/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
