f2177475ecf7ac2ab773ba4e0f0445ab435dc4db
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-09-25 14:48:58 -0500

    Improved timestamp formatting and rounded computations (4 decimal places)

--- pycalc/calculator/models.py
+++ pycalc/calculator/models.py
@@ -4,7 +4,9 @@ from datetime import datetime
 
 class Computation(models.Model):
     comp = models.TextField(max_length=30,default=None,blank=True,null=True)
-    entered_at = models.DateTimeField(default=datetime.now, blank=True)
+    t = datetime.now()
+    s = t.strftime("%c %Z")
+    entered_at = models.DateTimeField(default=s, blank=True)
     
     def __str__(self):
         return self.comp
