42b82a6382a220bb93d9990370d6d7309156fd00
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERs-MacBook-Pro.local>
2020-09-26 15:11:58 -0500

    Settings Update

--- pycalc/settings.py
+++ pycalc/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import redis
 # import django_heroku
-# import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,7 +140,7 @@ CHANNEL_LAYERS = {
     }
 }'''
 DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
