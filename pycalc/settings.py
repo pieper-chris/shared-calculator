4269265c631c9986623f28f143c025f3947629c7
CHRISTOPHER PIEPER <christopherpieper@CHRISTOPHERsMBP.hitronhub.home>
2020-10-22 16:32:48 -0500

    Automatic Heroku Redis Updates

--- pycalc/settings.py
+++ pycalc/settings.py
@@ -118,7 +118,7 @@ ASGI_APPLICATION = "pycalc.routing.application"
 
 
 # Must run redis server in background for this to work
-
+# When redis updates, use 'heroku config --app shared-global-calculator | grep REDIS'
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
@@ -126,7 +126,8 @@ CHANNEL_LAYERS = {
             "hosts": # old -> ["redis://h:pbb34988bf9712a7764cad15730a7724687e175fd1e65a8957013790b4d5b70a9@ec2-3-86-124-151.compute-1.amazonaws.com:8679"],
                      # old -> ["redis://h:pbb34988bf9712a7764cad15730a7724687e175fd1e65a8957013790b4d5b70a9@ec2-3-232-63-252.compute-1.amazonaws.com:32009"],
                      # old -> ["redis://h:pbb34988bf9712a7764cad15730a7724687e175fd1e65a8957013790b4d5b70a9@ec2-34-194-156-60.compute-1.amazonaws.com:24459"],
-                     ["redis://h:pbb34988bf9712a7764cad15730a7724687e175fd1e65a8957013790b4d5b70a9@ec2-184-73-174-176.compute-1.amazonaws.com:16289"],
+                     # old -> ["redis://h:pbb34988bf9712a7764cad15730a7724687e175fd1e65a8957013790b4d5b70a9@ec2-184-73-174-176.compute-1.amazonaws.com:16289"],
+                     ["redis://h:pbb34988bf9712a7764cad15730a7724687e175fd1e65a8957013790b4d5b70a9@ec2-52-3-150-117.compute-1.amazonaws.com:25819"],
         },
     },
 }
